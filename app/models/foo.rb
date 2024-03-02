d958daa677b76601af7b966ffbf682ccd2700c71
adamdawkins <adam@dragondrop.uk>
2024-03-02 00:01:17 +0000

    Add Foo#all_bars_yellow?

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,3 +1,7 @@
 class Foo < ApplicationRecord
   has_many :bars, dependent: :destroy
+
+  def all_bars_yellow?
+    bars.all?(&:yellow?)
+  end
 end
--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Foo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:foo) { described_class.new }
+  # The rules:
+  # You can't stub foo, it's the system under test
+  # this spec shouldn't have to change if the contents of `Bar#yellow?` change
+  describe "#all_bars_yellow?" do 
+    context "when all bars are yellow" do 
+      it "returns true" do
+        expect(foo.all_bars_yellow?).to be true
+      end
+    end
+
+    context "when some bars aren't yellow" do 
+      it "returns false" do
+        expect(foo.all_bars_yellow?).to be false
+      end
+    end
+
+  end
 end
