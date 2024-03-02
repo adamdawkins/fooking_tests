263fdfc4bc83c95ea6348a32ec6710cb7f24bd30
Ben Morrison <ben@flyht.uk>
2024-03-02 19:25:59 +0000

    Stub method on associations to allow them to pass

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -2,6 +2,8 @@ class Foo < ApplicationRecord
   has_many :bars, dependent: :destroy
 
   def all_bars_yellow?
+    return false if bars.empty?
+
     bars.all?(&:yellow?)
   end
 end
--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -5,18 +5,44 @@ RSpec.describe Foo, type: :model do
   # The rules:
   # You can't stub foo, it's the system under test
   # this spec shouldn't have to change if the contents of `Bar#yellow?` change
-  describe "#all_bars_yellow?" do 
-    context "when all bars are yellow" do 
-      it "returns true" do
-        expect(foo.all_bars_yellow?).to be true
+  #
+
+  describe '#all_bars_yellow?' do
+    context 'when there are no bars' do
+      it 'returns false' do
+        expect(foo.all_bars_yellow?).to eq false
       end
     end
 
-    context "when some bars aren't yellow" do 
-      it "returns false" do
-        expect(foo.all_bars_yellow?).to be false
+    context 'when there are bars' do
+      let(:first_bar) { Bar.new }
+      let(:second_bar) { Bar.new }
+
+      before do
+        foo.bars = [first_bar, second_bar]
+      end
+
+      context 'when all bars are yellow' do
+        before do
+          allow(first_bar).to receive(:yellow?).and_return(true)
+          allow(second_bar).to receive(:yellow?).and_return(true)
+        end
+
+        it 'returns true' do
+          expect(foo.all_bars_yellow?).to be true
+        end
+      end
+
+      context "when some bars aren't yellow" do
+        before do
+          allow(first_bar).to receive(:yellow?).and_return(true)
+          allow(second_bar).to receive(:yellow?).and_return(false)
+        end
+
+        it 'returns false' do
+          expect(foo.all_bars_yellow?).to eq false
+        end
       end
     end
-
   end
 end
